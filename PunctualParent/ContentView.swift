e8729f5b33272fbc05becd2f3cb60e35898aabf8
Elvin Bearden <elvin.bearden@gmail.com>
2020-08-10 10:52:27 -0500

    Initial Commit

---
+++ PunctualParent/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  PunctualParent
+//
+//  Created by Elvin Bearden on 8/10/20.
+//  Copyright © 2020 Elvin Bearden. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
